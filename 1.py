6be376d0a3538f0d7fd42ba17345fea9d73267c1
DimaD1989 <d_d1989sv@mail.ru>
2022-10-10 22:17:16 +0300

    4

--- 1.py
+++ 1.py
@@ -44,7 +44,21 @@
 # Пример:
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+# from random import random
+# from unittest import result
 
 
+# k=2
+# a=round(random()*10)
+# b=round(random()*10)
+# c=round(random()*10)
+# print(a,b,c)
+# result=""
+# if b==0: result= f"{a}*x**{k} + {c}*x**{k-1}=0"
+# if b==0 and c ==0: result= f"{a}*x**{k}=0"
+# if c==0: result= f"{a}*x**{k}+{b}*x**{k-1}=0"
+# else: result = f"{a}*x**{k}+{b}*x**{k-1}+{c}*x**{k-2}=0"
+# with open("file.txt", "w") as file1:
+#     file1.write(result)
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, 
 # содержащий сумму многочленов.
